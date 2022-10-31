83f29b6cfc4b4531659d25ce16ff95c4e6be8322
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2022-10-31 21:52:45 +0100

    register form now sends a hashed password to the db

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 const app = express();
 const User = require('./models/user');
+const bcrypt = require('bcrypt');
+
+mongoose
+  .connect('mongodb://localhost:27017/authDemo', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Mongo Connection Open!');
+  })
+  .catch((err) => {
+    console.log('Mongo Connection Error!');
+    console.log(err);
+  });
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.get('/register', (req, res) => {
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('This is the home page!');
+});
+
+app.get('/register', async (req, res) => {
   res.render('register');
 });
 
-app.get('/secret', (req, res) => {
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  const hash = await bcrypt.hash(password, 12);
+  //   console.log(hash);
+  const user = new User({ username, password: hash });
+  await user.save();
+  res.redirect('/');
+});
+
+app.get('/secret', async (req, res) => {
   res.send('This is a secret');
 });
 
