a2aba993f9224646f4b60623bce69003fa01d5c8
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2022-11-02 12:12:34 +0100

    setup middleware to require login

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ app.set('views', 'views');
 app.use(express.urlencoded({ extended: true }));
 app.use(session({ secret: 'notagoodsecret' }));
 
+const requireLogin = (req, res, next) => {
+  if (!req.session.user_id) {
+    return res.redirect('/login');
+  }
+  next();
+};
+
 app.get('/', (req, res) => {
   res.send('This is the home page!');
 });
@@ -67,11 +74,12 @@ app.post('/logout', (req, res) => {
   res.redirect('/login');
 });
 
-app.get('/secret', async (req, res) => {
-  if (!req.session.user_id) {
-    res.redirect('/login');
-  }
+app.get('/secret', requireLogin, (req, res) => {
   res.render('secret');
+});
+
+app.get('/topsecret', requireLogin, (req, res) => {
+  res.send('TOP SECRET!');
 });
 
 app.listen(3000, () => {
